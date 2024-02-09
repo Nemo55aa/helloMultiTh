e9ece8f91c7d1c8135fdfe91504af42d0a098515
Nemo55aa <minato.work1@gmail.com>
2024-02-10 00:31:25 +0900

    GUI(Swing)入門

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import myThread.helloTh;
+import myThread.worldTh;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        helloThread heTh = new helloThread();
-        worldThread wldTh = new worldThread();
+        MyFrame f = new MyFrame();
+        f.setVisible(true);
+
+        helloTh heTh = new helloTh();
+        worldTh wldTh = new worldTh();
         
         heTh.start();
         wldTh.start();
@@ -10,27 +21,33 @@ public class App {
     }
 }
 
-class helloThread extends Thread {
-    @Override
-    public void run(){
-        for(int i = 1; i <= 5; i++){
-            System.out.println("hello");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {}
-        }
+
+class MyFrame extends JFrame implements ActionListener{
+    JButton b1;
+    public MyFrame() {
+        Container contentPane = getContentPane();
+        b1 = new JButton("Button1");
+        
+        setSize(300, 200);
+        setTitle("My Window Application");
+        
+        b1.addActionListener(this);
+        contentPane.setLayout(new FlowLayout());
+        contentPane.add(b1);
+        addWindowListener(new MyWindowAdapter());
     }
     
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource() == b1) 
+           System.out.println("Button1がクリックされました");
+        }
 }
-class worldThread extends Thread {
-    @Override
-    public void run(){
-        for(int i = 1; i <= 5; i++){
-            System.out.println("world");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {}
-        }
+
+/**
+ * MyWindowAdapter extend
+ */
+class MyWindowAdapter extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+       System.exit(0);
     }
-    
 }
